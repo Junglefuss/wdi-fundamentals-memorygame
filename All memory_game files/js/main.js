a7c9ff724b1b548a56f12b581d88c3acfd3aa5cf
Jonathan Goldfuss <Jonathan@Jonathans-Air-3.fios-router.home>
2019-08-16 16:31:58 -0400

    created game logic

--- All memory_game files/js/main.js
+++ All memory_game files/js/main.js
@@ -1,16 +1,52 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamonds.png'
+}
+];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardsInPlay[0] + ".");
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
+};
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardsInPlay[1] + ".");
+var flipCard = function (cardId) {
+	cardsInPlay.push(cards[cardId].rank);
+		console.log("User flipped " + cards[cardId].rank + ".");
+checkForMatch();
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+};
 
-if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+
+
+flipCard(0);
+flipCard(1);
+
+
+/*if (cardsInPlay.length === 2 && cardOne === cardTwo) {
 	alert("You found a match!");
 } else {
 	alert("Sorry, try again.");
-}
+} */
+
